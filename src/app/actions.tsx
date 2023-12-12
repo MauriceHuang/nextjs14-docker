04712499ccd47c0a9fe50907271c919b64c17697
Maurice Huang <maurice.huang.pai.so@gmail.com>
2023-12-12 22:04:54 +0800

    #5 added delete button, revalidatePth(), home function convert to dynamic

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -2,8 +2,17 @@
 
 import db from "db";
 import { todos } from "db/schema";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
-export default async function createTodoAction(formData: FormData) {
+export async function createTodoAction(formData: FormData) {
     const text = formData.get("text") as string;
     await db.insert(todos).values({ text, })
+    revalidatePath("/");
+}
+
+export async function deleteTodoAction(formData: FormData) {
+    const id = parseInt(formData.get("id") as string);
+    await db.delete(todos).where(eq(todos.id, id));
+    revalidatePath("/");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import createTodoAction from "./actions";
-import db from "db";
-import { todos } from "db/schema";
+import { unstable_cache, unstable_noStore } from "next/cache";
+import { createTodoAction, deleteTodoAction } from "./actions";
+import { getAllTodos } from "data-access/todos";
 
 export default async function Home() {
-  const todos = await db.query.todos.findMany();
+  unstable_noStore();
+  const todos = await getAllTodos();
   return (
     <main className="">
       <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        {todos.map(todo => (
+          <li className="flex gap-2 item-center" key={todo.id}>{todo.text}
+            <form action={deleteTodoAction}>
+              <input type="hidden" name="id" value={todo.id} />
+              <button className="text-red-400">Delete</button>
+            </form>
+          </li>))}
       </ul>
       <form action={createTodoAction}>
         <input name="text" className="text-black" />
