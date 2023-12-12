54e609758dbd8d0ccef1cc2ac91ef3d82c2c408e
Maurice Huang <maurice.huang.pai.so@gmail.com>
2023-12-12 21:31:41 +0800

    showing todo items list via map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import createTodoAction from "./actions";
+import db from "db";
+import { todos } from "db/schema";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await db.query.todos.findMany();
   return (
     <main className="">
+      <ul>
+        {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+      </ul>
       <form action={createTodoAction}>
         <input name="text" className="text-black" />
         <button>Create Todo</button>
